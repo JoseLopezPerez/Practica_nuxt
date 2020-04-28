a9f18111714c17780efc33d818753df69976d1d6
Jose <joserep2510@gmail.com>
2020-04-27 22:33:19 -0500

    Agregando la ruta dinámica para productos/_id/index.vue

--- pages/productos.vue
+++ pages/productos.vue
@@ -7,6 +7,22 @@
        <v-btn to="/productos/10">
           producto 10
       </v-btn>
+      <v-card class="mx-auto"
+            max-width="322"
+            max-height="322"
+            color="primary"> 
+    <h1 class="black--text font-italic">productos pruebas</h1>
+    <h2 class="green--text">Titulo: Matriz Hertz</h2>
+    <label>Descripcion:</label>
+    <v-col>
+        <v-img height="200" width="310" 
+        src="https://cdn.vuetifyjs.com/images/cards/store.jpg">
+
+        </v-img>
+    </v-col>
+
+
+      </v-card>
       <nuxt-child></nuxt-child>
   </div>
 </template>
--- pages/productos/_id/index.vue
+++ pages/productos/_id/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-      PRODUCTO id: {{$route.params.id}}
+      
        <v-btn to="/productos">
           BACK
       </v-btn>
+
+      <h1> contenido 1.</h1>
+      <h1> contenido 2.</h1>
+      <h1> contenido 3.</h1>
+
+
+       
   </div>
 </template>
 
