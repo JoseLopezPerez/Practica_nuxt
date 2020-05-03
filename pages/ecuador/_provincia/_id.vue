8f880482566c4ff8cb6340e1d85dfb779b1dde5b
Jose <joserep2510@gmail.com>
2020-05-03 02:44:11 -0500

    Modificando el comentario de los objetos mal aninados

--- pages/ecuador/_provincia/_id.vue
+++ pages/ecuador/_provincia/_id.vue
@@ -5,9 +5,16 @@
         v-for="(nombreprincipal, index) in nombreprincipal"
         :key="index"
         v-text="
-          `${nombreprincipal.nombrecapitales} - ${nombreprincipal.nombreprincipalprovincia}`"></h1>
-      <li v-for="(provinciasycapitales, index) in provinciasycapitales" :key="index" > {{provinciasycapitales.capitales}}  - 
-        {{provinciasycapitales.provinciass}} </li>
+          `${nombreprincipal.nombrecapitales} - ${nombreprincipal.nombreprincipalprovincia}`
+        "
+      ></h1>
+      <li
+        v-for="(provinciasycapitales, index) in provinciasycapitales"
+        :key="index"
+      >
+        {{ provinciasycapitales.capitales }} -
+        {{ provinciasycapitales.provinciass }}
+      </li>
     </ul>
   </div>
 </template>
@@ -18,21 +25,20 @@ export default {
     nombreprincipal: [
       {
         nombrecapitales: 'Capitales',
-        nombreprincipalprovincia: 'Provincias',}
-        ],
-<<<<<<< HEAD
-        // CORREGIR EL OBJETO
-
-        
-=======
-        // CORREGIR EL OBJETO, anidar los elementos
->>>>>>> 435ecb68adacf612509acc6cca6ba0930a4c452c
-        provinciasycapitales:[
-          {capitales:'Guayaquil' ,provinciass:'Guayas'},
-          {capitales:'Quito ' ,provinciass:'Quito'},
-          {capitales:'Azuay ' ,provinciass:'Azuay'}
-        ]
-
+        nombreprincipalprovincia: 'Provincias'
+      }
+    ],
+    // CORREGIR EL OBJETO
+    provinciasycapitales: [
+      { capitales: 'Guayaquil', provinciass: 'Guayas' },
+      { capitales: 'Quito ', provinciass: 'Quito' },
+      { capitales: 'Azuay ', provinciass: 'Azuay' }
+    ],
+    provincias: [
+      { provinciass: 'Guayas' },
+      { provinciass: 'Quito' },
+      { provinciass: 'Azuay' }
+    ]
   })
 }
 </script>
